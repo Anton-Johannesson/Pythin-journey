8117406890d941a23eb78b9854a5e14023ec66d3
Anton-Johannesson <antonmma03@gmail.com>
2025-01-27 19:39:14 +0100

    First code, will make banking now

---
+++ firstCode.py
@@ -0,0 +1,99 @@
+#FUNCTIONS
+#def say_hello(name):
+    #print('Hey there', name)
+
+#say_hello('Ant')
+#say_hello('Brate')
+
+
+#def get_internet():
+    #pass
+#
+#def run_game():
+    #pass#makes the code run, even if it's empty of data
+
+number = input('Please provide a number >>')
+
+try: 
+    print(10 + int(number))
+except:
+    print('That is not a valid number!')
+    #Try and except block, makes the code try and run the "number" but if it cant, then it runs except block, kind of like if loops, where 
+    #the first condition might not be valid, then it tries the else if, and if that doesnt work, tries else
+
+
+
+#LOGIC STATESMENTS
+age = 21
+#if age > 18:
+    #print('You getting are old!')
+#elif age == 20:
+    #print('You are old!')
+#else:
+    #print('You are still young!')
+
+#for i in range(10):
+    #print('name?', i + 1)
+
+
+#nbr_list = [1, 10, 3, 6, 100, 50, 56, 30, 36, 29]
+
+#for nbr in nbr_list:
+    #print(nbr)
+
+#k = 0
+#while k < 10:
+#    k = k + 1
+#    print(k)
+
+#while True:
+    #user_input = input('Enter something >>')
+    #if user_input == '0':
+        #print('We are done here!!')
+        #break
+
+
+#MATH
+
+a = 10
+b = 15
+
+add = a + b
+diff = a - b
+multi = a * b
+div = a/b
+power = a**b
+
+#print(add)
+#print(diff)
+#print(multi)
+#print(div)
+#print(power)
+
+
+#STRING AND INTEGERS
+
+item = 'Wondering who I am?'
+item1 = 'Check next row'
+item_name = 'Computer Science'
+
+#print(item, item1)
+
+#print('Hello, this is what I study: ' + item_name)
+
+integer = 2025
+word = 'text'
+isHappy = False
+name = 'Secret'
+String_number = '21'
+
+#print(name + str(integer))
+#print(10 + int(String_number))
+
+
+#naughty_list = ['Me', 'not me']
+#print(integer)
+#print(word)
+#print(isHappy)
+#print(naughty_list)
+
